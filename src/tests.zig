1423b8de3c1bb99cc372eeed6272411770c6a8d2
Rasmus <rasmus.hyldgard.samsing@lego.com>
2024-08-04 00:08:30 +0200

    remove tests.zig

--- src/tests.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-pub const main = @import("main.zig");
-pub const rendering = @import("rendering/rendering.zig");
-pub const helpers = @import("helpers/helpers.zig");
-
-test {
-    std.testing.refAllDecls(@This());
-}
